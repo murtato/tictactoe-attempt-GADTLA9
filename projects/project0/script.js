c5ce0b8b49d848624fb5a089c8ba06e42fde444a
Victor Tran <murtato@gmail.com>
2016-03-14 10:46:24 -0700

    git status

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -25,8 +25,8 @@ function nextTurn() {
 var grid;
 var player;
 var winConditions = [[0,1,2], [3,4,5], [6,7,8],
-                [0,3,6], [1,4,7], [2,5,8],
-                [0,4,8], [6,4,2]]
+                    [0,3,6], [1,4,7], [2,5,8],
+                    [0,4,8], [6,4,2]]
 var turn = 'X';
 var score = {
   'X' = 0,
